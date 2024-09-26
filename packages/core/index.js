2b4c8895d53c5ebc2252dee30b19561295a61945
WMC <1375298047@qq.com>
2024-09-26 13:33:15 +0800

    补充提交

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,6 @@
 import { makeInstaller } from '@usercomp/utils'
 import components from './component.js'
+import '@usercomp/theme/index.css'
 
 // 注册所有组件 并返回为 mian.js 中app.use做准备
 const installer = makeInstaller(components)
